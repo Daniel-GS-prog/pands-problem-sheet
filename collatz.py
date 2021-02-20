ec047585e24896a0fc4ae2886ad63f0a5d670184
Daniel Gonzalez <daniel_gonzalez_sanchez@yahoo.com>
2021-02-20 14:38:37 +0000

    Cleaning up comments

--- collatz.py
+++ collatz.py
@@ -8,18 +8,27 @@
 
 
 
-i = int(input(': ')) # takes input from user
-numbers = []        # empty list to be populated by the loop
-numbers.append(i)   # first character of the list
-x = i               # sets user's input to variable in loop
+i = int(input(': ')) 
+# takes input from user
+numbers = []    
+# empty list to be populated by the loop    
+numbers.append(i)   
+# first character of the list
+x = i
+# sets user's input to variable in loop
+
 
 while x != 1:
-    if x % 2 == 0:          #condition one
-        numbers.append(x/2) # list appends character
-        x = x/2             # updates value of x
-    else:                           # condition two
-        numbers.append(x * 3 + 1)   # list appends character
-        x= x * 3 + 1                # updates the value of x
+    if x % 2 == 0:         
+        numbers.append(x/2) 
+        # list appends character
+        x = x/2             
+        # updates value of x
+    else:                           
+        numbers.append(x * 3 + 1)   
+        # list appends character
+        x= x * 3 + 1                
+        # updates the value of x
     
 print(numbers) # Prints results
 
