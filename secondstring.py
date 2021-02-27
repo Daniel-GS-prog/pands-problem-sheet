10139277575acfe3ddb1bd45651f78b550227d30
Daniel Gonzalez <daniel_gonzalez_sanchez@yahoo.com>
2021-02-27 11:39:11 +0000

    Returns a string backwards skipping one position

---
+++ secondstring.py
@@ -0,0 +1,10 @@
+# Takes in a string and outputs every second letter in reverse order
+# Author: Daniel Gonzalez
+
+
+## -- asks user for a string -- ##
+string1 = "The quick brown fox jumps over the lazy dog."
+
+## -- Prints the string backwards skiping one position -- ##
+print(string1[-1::-2])
+
