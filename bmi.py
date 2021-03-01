93c508e0513952cfcde65819254ca45debc54d10
Daniel Gonzalez <daniel_gonzalez_sanchez@yahoo.com>
2021-03-01 15:06:28 +0000

    Updated with function

--- bmi.py
+++ bmi.py
@@ -2,26 +2,28 @@
 # Takes the user´s height in (meters) and weight (in kg) and returs the body mass index
 # Author: Daniel Gonzalez
 
-print(('\n-------------------------------'))
-print('Welcome to he BMI calculator!')
 
-name = input('\nWhat´s your name: ')
-print('Hello, {}. Nice to meet you!'. format(name))
 
-#User´s values:
-weight = float(input('\nPlease enter your weight in kg: '))
-height = float(input('Please enter your height in meters: '))
-bmi = weight / (height**2)
 
-#Returns user´s BMI:
-print('\nYour Body Mass Index is: {0:.2f}'. format(bmi))
+def bmi(name, height, weight):
 
-#cathegories:
-if bmi < 18.5:
-    print('Your bmi is a little too low. Please go eat something.')
-elif 18.5 <= bmi <= 24.9:
-    print('Ypur bmi is at healthy levels! Congrats')
-else:
-    print('Your bmi is a bit too high.')
-print('------------------------------')
+    print(('\n-------------------------------'))
+    bmi = weight / (height**2)
+    userBmi = round(bmi, 2)
+    # All values and calulations
+    if userBmi < 18.5: 
+    # Comparions and results returned
+        return "{}, your bmi is {}. It's a little too low. Please go eat something.\n.".format(name, userBmi)
+    elif 18.5 <= userBmi <= 24.9:
+        return "{}, your bmi is {}. It's at healthy levels! Congrats.\n".format(name, userBmi)
+    else:
+        return "{}, your bmi is {}. It's a bit too high.\n".format(name, userBmi)
+    print('------------------------------')
+            
+
+print(bmi(input('\nEnter your name: '), float(input('enter your height in meters: ')), float(input('enter your weight in kg: '))))
+
+
+
+
     
