cf0c859eb313dfff1ece3fd43b8676f1ed9710aa
Daniel Gonzalez <daniel_gonzalez_sanchez@yahoo.com>
2021-03-29 15:47:31 +0100

    Updated version with functions and changes made as per professor's observations

--- bmi.py
+++ bmi.py
@@ -1,29 +1,52 @@
 
-# Takes the user´s height in (meters) and weight (in kg) and returs the body mass index
+# Takes the user's height in (meters) and weight (in kg).
+# returs the body mass index and classification.
 # Author: Daniel Gonzalez
 
 
-
-
-def bmi(name, height, weight):
-
+def main():
+    # Calls all other functions
+    # Returs user's BMI index and classification
     print(('\n-------------------------------'))
-    bmi = weight / (height**2)
-    userBmi = round(bmi, 2)
-    # All values and calulations
-    if userBmi < 18.5: 
-    # Comparions and results returned
-        return "{}, your bmi is {}. It's a little too low. Please go eat something.\n.".format(name, userBmi)
-    elif 18.5 <= userBmi <= 24.9:
-        return "{}, your bmi is {}. It's at healthy levels! Congrats.\n".format(name, userBmi)
-    else:
-        return "{}, your bmi is {}. It's a bit too high.\n".format(name, userBmi)
-    print('------------------------------')
-            
-
-print(bmi(input('\nEnter your name: '), float(input('enter your height in meters: ')), float(input('enter your weight in kg: '))))
-
-
-
-
+    name = get_name()
+    height = get_height()
+    weight = get_weight()
+    userBmi = bmi(height, weight)
+    bmiRange = bmiClassification(userBmi, name)
+    return bmiRange
     
+
+def get_name():
+    # Requests and returns user's name
+    return input("Enter name: ")
+
+
+def get_height():
+    # Requests and returns user's height
+    return float(input("Enter height in meters: "))
+
+
+def get_weight():
+    # Requests and returns user's weight
+    return float(input("Enter weight in kg: "))
+
+
+def bmi(height, weight):
+    # calculates bmi index from user's input (height and weight)
+    # Retuns index limited to 2 decimal places
+    a = weight / (height**2)
+    return round(a, 2)
+
+
+def bmiClassification(bmi, name):
+    # Compares user's bmi with range.
+    # Retuns low, normal or hight bmi index
+    if bmi < 18.5: 
+        return "\n{}, your bmi is {}. It's a little too low. Please go eat something.\n.".format(name, bmi)
+    elif 18.5 <= bmi <= 24.9:
+        return "\n{}, your bmi is {}. It's at healthy levels! Congrats.\n".format(name, bmi)
+    else:
+        return "\n{}, your bmi is {}. It's a bit too high.\n".format(name, bmi)
+
+
+print(main())
